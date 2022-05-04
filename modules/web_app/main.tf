6729f4ec8a10ff15c5749e050dc3426b9e646999
saito-hiroaki <hiro021312@gmail.com>
2022-05-04 23:50:34 +0900

    [add]webappの雛形module新規作成

---
+++ modules/web_app/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_app_service_plan" "common_module_app_service_plan" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "common_module_app_service" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+}
