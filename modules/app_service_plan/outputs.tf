80057e1602d5bb3c26b5563b404f89cdde31778b
saito-hiroaki <hiro021312@gmail.com>
2022-05-05 15:50:14 +0900

    [add]app_service_planのmodule追加

---
+++ modules/app_service_plan/outputs.tf
@@ -0,0 +1,4 @@
+output "app_service_plan_id" {
+  value = azurerm_app_service_plan.common_module_app_service_plan.app_service_plan_id
+
+}
--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -1,14 +1,3 @@
-resource "azurerm_app_service_plan" "common_module_app_service_plan" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
 resource "azurerm_app_service" "common_module_app_service" {
   name                = "example-app-service"
   location            = azurerm_resource_group.example.location
